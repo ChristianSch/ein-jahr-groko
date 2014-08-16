914e96c0d00b2b9e8e9d5fe9008b08c975b672df
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-16 11:09:52 +0200

    updated appTitle and introduced baseUri for api requests

--- assets/js/values.js
+++ assets/js/values.js
@@ -2,4 +2,5 @@
 
 
 angular.module('myApp.values', [])
-    .value('appTitle', 'Super awesome webapp');
+    .value('appTitle', 'Ein Jahr GroKo')
+    .value('dataBaseUri', 'http://www.corsproxy.com/wahlversprechen2013.de/json/');
