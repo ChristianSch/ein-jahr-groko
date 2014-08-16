018713a331aa3a7b61fe653d04a26bc40241c36a
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-16 11:10:24 +0200

    beautified

--- assets/js/services.js
+++ assets/js/services.js
@@ -2,43 +2,47 @@
 
 
 angular.module('myApp.services', [])
-	.factory('RandomIDService', function() {
-		/**
-		 * Return random integer between min and max (both inclusive)
-		 * @param  {int} min minimal value
-		 * @param  {int} max maximal value
-		 * @return {int}     random int from `[min, max]`
-		 */
-		function getRandID(min, max) {
-			return Math.floor(((Math.random() * (max - min) + 1) + min) + 1);
-		}
+    .factory('RandomIDService', function() {
+        /**
+         * Return random integer between min and max (both inclusive)
+         * @param  {int} min minimal value
+         * @param  {int} max maximal value
+         * @return {int}     random int from `[min, max]`
+         */
+        function getRandID(min, max) {
+            return Math.floor(((Math.random() * (max - min) + 1) + min) + 1);
+        }
 
-		return {
-			getRandID: getRandID
-		};
-	})
-	.factory('RandomQuoteService', ['$http', '$q', 'RandomIDService', 'dataBaseUri',
-		function($http, $q, RandomIDService, dataBaseUri) {
-			function getRandQuote(){
-				var defer = $q.defer;
-				var lowestID = 0;
-				var highestID = 1173; // FIXME: shouldn't be hardcoded
-				var randID = RandomIDService.getRandID(lowestID, highestID);
+        return {
+            getRandID: getRandID
+        };
+    })
+    .factory('RandomQuoteService', ['$http', '$q', 'RandomIDService', 'dataBaseUri',
+        function($http, $q, RandomIDService, dataBaseUri) {
+            function getRandQuote() {
+                var defer = $q.defer;
+                var lowestID = 0;
+                var highestID = 1173; // FIXME: shouldn't be hardcoded
+                var randID = RandomIDService.getRandID(lowestID, highestID);
 
-				$http({ 'method': 'GET', 'url': dataBaseUri + 'item/' + randID})
-					.success(function(data, status, headers, config) {
-						console.log(data);
-						return {};
-					})
-					.error(function(data, status, headers, config) {
-						console.log("returned status " + status);
-						return {};
-					});
+                $http({
+                    'method': 'GET',
+                    'url': dataBaseUri + 'item/' + randID
+                })
+                    .success(function(data, status, headers, config) {
+                        console.log(data);
+                        return {};
+                    })
+                    .error(function(data, status, headers, config) {
+                        console.log("returned status " + status);
+                        return {};
+                    });
 
-				return defer.promise;
-			}
+                return defer.promise;
+            }
 
-			return {
-				getRandQuote: getRandQuote
-			};
-		}]);
+            return {
+                getRandQuote: getRandQuote
+            };
+        }
+    ]);
