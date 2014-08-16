6d7f20420c8fa910eef9f86650df58ac8d3df5b2
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-16 11:15:00 +0200

    implemented RandomQuoteCtrl with example data

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -6,4 +6,15 @@ angular.module('myApp.controllers', [])
         function($scope, $window, appTitle) {
             $window.document.title = appTitle;
         }
+    ])
+    .controller('RandomQuoteCtrl', ['$scope', 'RandomQuoteService',
+        function($scope, RandomQuoteService) {
+            RandomQuoteService.getRandQuote(); //.then();
+            $scope.quote = {
+                'title': 'Foo Bar!',
+                'author': 'George Washington',
+                'text': 'Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam quis risus eget urna mollis ornare vel eu leo.',
+                'source': 'google.de'
+            };
+        }
     ]);
