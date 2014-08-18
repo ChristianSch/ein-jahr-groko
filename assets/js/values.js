c4dd2fbb4fc4e15fc9e896d1e576d70e6482bfd0
Christian Schulze <christian.schulze@mni.thm.de>
2014-08-18 12:53:17 +0200

    fixed #1 and #2
    + the service now returns the $http promise directly
    + fixed the url
    random quotes now working (on refresh)

--- assets/js/values.js
+++ assets/js/values.js
@@ -3,4 +3,4 @@
 
 angular.module('myApp.values', [])
     .value('appTitle', 'Ein Jahr GroKo')
-    .value('dataBaseUri', 'http://www.corsproxy.com/wahlversprechen2013.de/json/');
+    .value('dataBaseUri', 'http://www.wahlversprechen2013.de/json/');
